42de72a4567a1636d08d36713d9995a14dbbc25e
Ed Hickey <ed@ed-new-laptop.local>
2008-09-03 17:45:31 -0500

    added in the Collection class from the main tree

---
+++ lib/thinking_sphinx/collection.rb
@@ -0,0 +1,23 @@
+module ThinkingSphinx
+  class Collection < ::Array
+    attr_reader :total_entries, :total_pages, :current_page
+    
+    def initialize(page, per_page, entries, total_entries)
+      @current_page, @per_page, @total_entries = page, per_page, total_entries
+      
+      @total_pages = (entries / @per_page.to_f).ceil
+    end
+    
+    def previous_page
+      current_page > 1 ? (current_page - 1) : nil
+    end
+    
+    def next_page
+      current_page < total_pages ? (current_page + 1): nil
+    end
+    
+    def offset
+      (current_page - 1) * @per_page
+    end
+  end
+end
