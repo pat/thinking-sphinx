26b35f660b04a791d71169243d53e7517cfea522
Mike Flester <mike@psalm8.local>
2008-09-08 10:21:30 -0400

    select core index name in a consistent way

--- lib/thinking_sphinx/active_record.rb
+++ lib/thinking_sphinx/active_record.rb
@@ -118,7 +118,7 @@ module ThinkingSphinx
     end
     
     def in_core_index?
-      @in_core_index ||= self.class.search_for_id(self.id, "#{self.class.name.downcase}_core")
+      @in_core_index ||= self.class.search_for_id(self.id, "#{self.class.name.underscore}_core")
     end
     
     def toggle_deleted
