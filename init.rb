309cb086382af0321acc92b489afbfa75aaa3590
Pat Allan <pat@freelancing-gods.com>
2008-10-12 20:58:42 +0700

    Adding manual load_models call for Rails pre 2.1 - the other prepare catch only happens on reloads, not initial load (unlike 2.1)

--- init.rb
+++ init.rb
@@ -1,6 +1,10 @@
 require 'thinking_sphinx'
 require 'action_controller/dispatcher'
 
+if Rails::VERSION::STRING.to_f < 2.1
+  ThinkingSphinx::Configuration.new.load_models
+end
+
 ActionController::Dispatcher.to_prepare :thinking_sphinx do
   ThinkingSphinx::Configuration.new.load_models
 end
