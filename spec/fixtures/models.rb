fdb963434fe34bb1cfb44336ea022b54d5e4825d
Pat Allan <pat@freelancing-gods.com>
2008-10-12 11:20:22 +0700

    Syntax tweak to avoid deprecation message in 1.2.6

--- spec/fixtures/models.rb
+++ spec/fixtures/models.rb
@@ -49,7 +49,7 @@ end
 
 class Friendship < ActiveRecord::Base
   belongs_to :person
-  belongs_to :friend, :class_name => "Person"
+  belongs_to :friend, :class_name => "Person", :foreign_key => :friend_id
   
   define_index do
     has person_id, friend_id
