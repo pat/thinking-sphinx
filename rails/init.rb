a2fc54d841658d1c4d49f53890142d35b1bd1e2d
Pat Allan <pat@freelancing-gods.com>
2008-11-09 13:32:00 +0700

    Adding load_models calls to rails/init.rb, as Rails 2.2 ignores /init.rb now

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,6 @@
 require 'thinking_sphinx'
+require 'action_controller/dispatcher'
+
+ActionController::Dispatcher.to_prepare :thinking_sphinx do
+  ThinkingSphinx::Configuration.instance.load_models
+end
