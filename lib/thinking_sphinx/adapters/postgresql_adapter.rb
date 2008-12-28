d176a13440eece16a4790c2a429f36254d5c71c9
Pat Allan <pat@freelancing-gods.com>
2008-12-28 12:51:22 +0700

    Postgres Adapter restructure and bug fix for the crc32 function when using PG8.2

--- lib/thinking_sphinx/adapters/postgresql_adapter.rb
+++ lib/thinking_sphinx/adapters/postgresql_adapter.rb
@@ -8,31 +8,11 @@ module ThinkingSphinx
       
       private
       
-      def create_array_accum_function
+      def execute(command)
         connection.execute "begin"
         connection.execute "savepoint ts"
         begin
-          # See http://www.postgresql.org/docs/8.2/interactive/sql-createaggregate.html
-          if connection.raw_connection.server_version > 80200
-            connection.execute <<-SQL
-              CREATE AGGREGATE array_accum (anyelement)
-              (
-                  sfunc = array_append,
-                  stype = anyarray,
-                  initcond = '{}'
-              );
-            SQL
-          else
-            connection.execute <<-SQL
-              CREATE AGGREGATE array_accum
-              (
-                  basetype = anyelement,
-                  sfunc = array_append,
-                  stype = anyarray,
-                  initcond = '{}'
-              );
-            SQL
-          end
+          connection.execute command
         rescue
           connection.execute "rollback to savepoint ts"
         end
@@ -40,44 +20,61 @@ module ThinkingSphinx
         connection.execute "commit"
       end
       
+      def create_array_accum_function
+        if connection.raw_connection.server_version > 80200
+          execute <<-SQL
+            CREATE AGGREGATE array_accum (anyelement)
+            (
+                sfunc = array_append,
+                stype = anyarray,
+                initcond = '{}'
+            );
+          SQL
+        else
+          execute <<-SQL
+            CREATE AGGREGATE array_accum
+            (
+                basetype = anyelement,
+                sfunc = array_append,
+                stype = anyarray,
+                initcond = '{}'
+            );
+          SQL
+        end
+      end
+      
       def create_crc32_function
-        connection.execute "begin"
-        connection.execute "savepoint ts"
-        begin
-          connection.execute "CREATE LANGUAGE 'plpgsql';"
-          connection.execute <<-SQL
-            CREATE OR REPLACE FUNCTION crc32(word text)
-            RETURNS bigint AS $$
-              DECLARE tmp bigint;
-              DECLARE i int;
-              DECLARE j int;
-              BEGIN
-                i = 0;
-                tmp = 4294967295;
+        # execute "CREATE LANGUAGE 'plpgsql';"
+        execute <<-SQL
+          CREATE OR REPLACE FUNCTION crc32(word text)
+          RETURNS bigint AS $$
+            DECLARE tmp bigint;
+            DECLARE i int;
+            DECLARE j int;
+            DECLARE word_array bytea;
+            BEGIN
+              i = 0;
+              tmp = 4294967295;
+              word_array = decode(replace(word, E'\\\\', E'\\\\\\\\'), 'escape');
+              LOOP
+                tmp = (tmp # get_byte(word_array, i))::bigint;
+                i = i + 1;
+                j = 0;
                 LOOP
-                  tmp = (tmp # get_byte(word::bytea, i))::bigint;
-                  i = i + 1;
-                  j = 0;
-                  LOOP
-                    tmp = ((tmp >> 1) # (3988292384 * (tmp & 1)))::bigint;
-                    j = j + 1;
-                    IF j >= 8 THEN
-                      EXIT;
-                    END IF;
-                  END LOOP;
-                  IF i >= char_length(word) THEN
+                  tmp = ((tmp >> 1) # (3988292384 * (tmp & 1)))::bigint;
+                  j = j + 1;
+                  IF j >= 8 THEN
                     EXIT;
                   END IF;
                 END LOOP;
-                return (tmp # 4294967295);
-              END
-            $$ IMMUTABLE STRICT LANGUAGE plpgsql;
-          SQL
-        rescue
-          connection.execute "rollback to savepoint ts"
-        end
-        connection.execute "release savepoint ts"
-        connection.execute "commit"
+                IF i >= char_length(word) THEN
+                  EXIT;
+                END IF;
+              END LOOP;
+              return (tmp # 4294967295);
+            END
+          $$ IMMUTABLE STRICT LANGUAGE plpgsql;
+        SQL
       end
     end
   end
