5da6538898a5500fafa1143df835dfb30c4adf75
Pat Allan <pat@freelancing-gods.com>
2009-01-03 00:56:02 +0700

    One more 1.2.x fix

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'thinking_sphinx'
-require 'action_controller/dispatcher'
 
 if Rails::VERSION::STRING.to_f < 2.1
   ThinkingSphinx::Configuration.instance.load_models
 end
 
-ActionController::Dispatcher.to_prepare :thinking_sphinx do
-  ThinkingSphinx::Configuration.instance.load_models
+if Rails::VERSION::STRING.to_f > 1.2
+  require 'action_controller/dispatcher'
+  ActionController::Dispatcher.to_prepare :thinking_sphinx do
+    ThinkingSphinx::Configuration.instance.load_models
+  end
 end
