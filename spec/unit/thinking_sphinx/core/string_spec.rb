672bf6d60a998d159099faca5e69e69fdf618763
Lourens Naude <lourens@methodmissing.com>
2009-01-09 18:52:17 +0000

    Omitted files from previous commit

---
+++ spec/unit/thinking_sphinx/core/string_spec.rb
@@ -0,0 +1,9 @@
+require 'spec/spec_helper'
+
+describe String do  
+  describe "to_crc32 instance method" do
+    it "should return an integer" do
+      'to_crc32'.to_crc32.should be_a_kind_of(Integer)
+    end
+  end
+end    
