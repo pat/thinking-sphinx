5c7cbe019257b34fd02e0d3581f357717ea48fbd
Pat Allan <pat@freelancing-gods.com>
2009-03-09 23:10:21 +1100

    Switching to Zlib's CRC implementation - native C, so much much much faster. Thanks again to Chris Heald for suggesting this and doing the hard yards on benchmarking.

--- lib/thinking_sphinx/core/string.rb
+++ lib/thinking_sphinx/core/string.rb
@@ -1,18 +1,11 @@
+require 'zlib'
+
 module ThinkingSphinx
   module Core
     module String
-      
       def to_crc32
-        result = 0xFFFFFFFF
-        self.each_byte do |byte|
-          result ^= byte
-          8.times do
-            result = (result >> 1) ^ (0xEDB88320 * (result & 1))
-          end
-        end
-        result ^ 0xFFFFFFFF
+        Zlib.crc32 self
       end
-      
     end
   end
 end
