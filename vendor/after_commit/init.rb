c4e185dedeadc88a66143c43f0585605a2985ecc
Laszlo Bacsi <lackac@lackac.hu>
2009-03-18 16:11:55 +0100

    Fixed transaction method chaining incompatilibty with jdbc-adapter

--- vendor/after_commit/init.rb
+++ vendor/after_commit/init.rb
@@ -3,3 +3,6 @@ ActiveRecord::Base.send(:include, AfterCommit::ActiveRecord)
 Object.subclasses_of(ActiveRecord::ConnectionAdapters::AbstractAdapter).each do |klass|
   klass.send(:include, AfterCommit::ConnectionAdapters)
 end
+if defined?(JRUBY_VERSION) and defined?(JdbcSpec::MySQL)
+  JdbcSpec::MySQL.send :include, AfterCommit::ConnectionAdapters
+end
