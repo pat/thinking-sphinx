897671c2fdfb4093422352d8cab774cf8e9f2b8d
Pat Allan <pat@freelancing-gods.com>
2009-05-09 14:37:48 -0700

    Adding forced-load of I18n to ensure translations are loaded before models. Thanks to Dr Mark Lane

--- rails/init.rb
+++ rails/init.rb
@@ -2,5 +2,13 @@ require 'thinking_sphinx'
 require 'action_controller/dispatcher'
 
 ActionController::Dispatcher.to_prepare :thinking_sphinx do
+  # Force internationalisation to be loaded.
+  if Rails::VERSION::STRING.to_f > 2.2
+    I18n.backend.reload!
+    I18n.backend.available_locales
+  elsif Rails::VERSION::STRING.to_f > 2.1
+    I18n.backend.load_translations(*I18n.load_path)
+  end
+  
   ThinkingSphinx::Configuration.instance.load_models
 end
