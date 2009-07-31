1f4c7b9dfd72a037994a4bb2f05eb2817e77ae3f
Pat Allan <pat@freelancing-gods.com>
2009-07-31 21:40:37 +0100

    Neatening up Ginger Scenario file (and setting 2.3 to use 2.3.3 instead of 2.3.2)

--- ginger_scenarios.rb
+++ ginger_scenarios.rb
@@ -4,25 +4,25 @@ Ginger.configure do |config|
   config.aliases["active_record"] = "activerecord"
   config.aliases["active_support"] = "activesupport"
   
-  ar_1_2_6 = Ginger::Scenario.new
-  ar_1_2_6[/^active_?support$/] = "1.4.4"
-  ar_1_2_6[/^active_?record$/] = "1.15.6"
+  ar_1_2 = Ginger::Scenario.new # 1.2.6
+  ar_1_2[/^active_?support$/] = "1.4.4"
+  ar_1_2[/^active_?record$/] = "1.15.6"
   
-  ar_2_0_4 = Ginger::Scenario.new
-  ar_2_0_4[/^active_?support$/] = "2.0.4"
-  ar_2_0_4[/^active_?record$/] = "2.0.4"
+  ar_2_0 = Ginger::Scenario.new
+  ar_2_0[/^active_?support$/] = "2.0.4"
+  ar_2_0[/^active_?record$/] = "2.0.4"
   
-  ar_2_1_2 = Ginger::Scenario.new
-  ar_2_1_2[/^active_?support$/] = "2.1.2"
-  ar_2_1_2[/^active_?record$/] = "2.1.2"
+  ar_2_1 = Ginger::Scenario.new
+  ar_2_1[/^active_?support$/] = "2.1.2"
+  ar_2_1[/^active_?record$/] = "2.1.2"
   
-  ar_2_2_0 = Ginger::Scenario.new
-  ar_2_2_0[/^active_?support$/] = "2.2.0"
-  ar_2_2_0[/^active_?record$/] = "2.2.0"
+  ar_2_2 = Ginger::Scenario.new
+  ar_2_2[/^active_?support$/] = "2.2.0"
+  ar_2_2[/^active_?record$/] = "2.2.0"
   
-  ar_2_3_0 = Ginger::Scenario.new
-  ar_2_3_0[/^active_?support$/] = "2.3.0"
-  ar_2_3_0[/^active_?record$/] = "2.3.0"
+  ar_2_3 = Ginger::Scenario.new
+  ar_2_3[/^active_?support$/] = "2.3.3"
+  ar_2_3[/^active_?record$/] = "2.3.3"
   
-  config.scenarios << ar_1_2_6 << ar_2_0_4 << ar_2_1_2 << ar_2_2_0 << ar_2_3_0
+  config.scenarios << ar_1_2 << ar_2_0 << ar_2_1 << ar_2_2 << ar_2_3
 end
