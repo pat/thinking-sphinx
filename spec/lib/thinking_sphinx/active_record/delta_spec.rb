9b1a43384aa5f6c07564fe7158cb3503cc82cf88
Ward Bekker <ward@tty.nl>
2009-08-11 14:46:08 +0200

    removed unfinished method

--- spec/lib/thinking_sphinx/active_record/delta_spec.rb
+++ spec/lib/thinking_sphinx/active_record/delta_spec.rb
@@ -19,14 +19,6 @@ describe "ThinkingSphinx::ActiveRecord::Delta" do
     @beta.should have_received(:toggle_delta)
   end
   
-  it "should call the toggle_delta on associated instances" do
-    
-    #if parent changes the first name, all childs should also toggle the delta
-   # @parent = Parent.
-    
-    
-  end
-
   describe "suspended_delta method" do
     before :each do
       ThinkingSphinx.stub_method(:deltas_enabled? => true)
