99f144bcc4fd9e6ca4f09a0dc88020b150dbe022
Joost Hietbrink <joost@truvolabs.com>
2009-09-18 10:37:39 +0200

    Added some documentation on scopes.

--- lib/thinking_sphinx/active_record/scopes.rb
+++ lib/thinking_sphinx/active_record/scopes.rb
@@ -8,10 +8,36 @@ module ThinkingSphinx
       end
     
       module ClassMethods
+        
+        # Similar to ActiveRecord's default_scope method Thinking Sphinx supports
+        # a default_sphinx_scope. For example:
+        #
+        #    default_sphinx_scope {
+        #         { :order => 'created_at DESC' }
+        #       }
+        #
+        # The scope is automatically applied when the search method is called.
+        # The default_scope can also be created using:
+        #
+        #   sphinx_scope(:default) {
+        #         { :order => 'created_at DESC' }
+        #      }
+        #
         def default_sphinx_scope(&block)
           sphinx_scope(:default, &block)
         end
 
+        # Similar to ActiveRecord's named_scope method Thinking Sphinx supports
+        # scopes. For example:
+        #
+        #   sphinx_scope(:latest_first) { 
+        #       {:order => 'created_at DESC, @relevance DESC'}
+        #     }
+        #
+        # Usage:
+        #
+        #   @articles =  Article.latest_first.search 'pancakes'
+        #
         def sphinx_scope(method, &block)
           @sphinx_scopes ||= []
           @sphinx_scopes << method
@@ -25,7 +51,9 @@ module ThinkingSphinx
             end
           end
         end
-        
+
+        # This returns an Array of all defined scopes. The default
+        # scope shows as :default.
         def sphinx_scopes
           @sphinx_scopes || []
         end
