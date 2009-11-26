670de0495718bbef762acbd0bd215589237831f0
Pat Allan <pat@freelancing-gods.com>
2009-11-26 13:22:46 +1100

    Using vendored versions of gems for cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,9 @@ require 'will_paginate'
 require 'active_record'
 
 $:.unshift File.dirname(__FILE__) + '/../../lib'
+Dir[File.join(File.dirname(__FILE__), '../../vendor/*/lib')].each do |path|
+  $:.unshift path
+end
 
 require 'cucumber/thinking_sphinx/internal_world'
 
