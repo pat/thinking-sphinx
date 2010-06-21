d722211e4176d6dfca7befc3f6bf96effbf385cb
Pat Allan <pat@freelancing-gods.com>
2010-06-21 17:18:20 +1000

    Switching metaclass references to singleton_class.

--- lib/thinking_sphinx/search.rb
+++ lib/thinking_sphinx/search.rb
@@ -294,7 +294,7 @@ module ThinkingSphinx
         excerpter = ThinkingSphinx::Excerpter.new self, object
         block = lambda { excerpter }
         
-        object.metaclass.instance_eval do
+        object.singleton_class.instance_eval do
           define_method(:excerpts, &block)
         end
       end
@@ -307,7 +307,7 @@ module ThinkingSphinx
         match = match_hash object
         next if match.nil?
         
-        object.metaclass.instance_eval do
+        object.singleton_class.instance_eval do
           define_method(:sphinx_attributes) { match[:attributes] }
         end
       end
@@ -323,7 +323,7 @@ module ThinkingSphinx
           @results[:fields], match[:weight]
         )
         
-        object.metaclass.instance_eval do
+        object.singleton_class.instance_eval do
           define_method(:matching_fields) { fields }
         end
       end
