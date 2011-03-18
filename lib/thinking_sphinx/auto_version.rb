6db04e3547797525b6d2031f7cbc7ee734fecfa1
Greg Weber <greg@gregweber.info>
2011-03-18 06:21:25 -0700

    allow sphinx vesion that is compiled from source

--- lib/thinking_sphinx/auto_version.rb
+++ lib/thinking_sphinx/auto_version.rb
@@ -5,9 +5,12 @@ module ThinkingSphinx
       case version
       when '0.9.8', '0.9.9'
         require "riddle/#{version}"
-      when '1.10-beta', '1.10-id64-beta'
+      when '1.10-beta', '1.10-id64-beta', '1.10-dev'
         require 'riddle/1.10'
       else
+        unless version.nil? or version.empty?
+          STDERR.puts "Unsupported version: #{version}"
+        end
         STDERR.puts %Q{
 Sphinx cannot be found on your system. You may need to configure the following
 settings in your config/sphinx.yml file:
