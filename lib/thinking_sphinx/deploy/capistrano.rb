7b3709c0e198ee1a06b7e6f7e2402eb546c02276
Konstantin Shabanov <etehtsea@gmail.com>
2011-04-14 01:29:52 +0700

    whitespace strip

--- lib/thinking_sphinx/deploy/capistrano.rb
+++ lib/thinking_sphinx/deploy/capistrano.rb
@@ -3,13 +3,13 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :install do
       desc <<-DESC
         Install Sphinx by source
-        
+
         If Postgres is available, Sphinx will use it.
-        
+
         If the variable :thinking_sphinx_configure_args is set, it will
         be passed to the Sphinx configure script. You can use this to
         install Sphinx in a non-standard location:
-        
+
           set :thinking_sphinx_configure_args, "--prefix=$HOME/software"
 DESC
 
@@ -22,7 +22,7 @@ DESC
         rescue Capistrano::CommandError => e
           puts "Continuing despite error: #{e.message}"
         end
-      
+
         args = []
         if with_postgres
           run "pg_config --pkgincludedir" do |channel, stream, data|
@@ -30,7 +30,7 @@ DESC
           end
         end
         args << fetch(:thinking_sphinx_configure_args, '')
-        
+
         commands = <<-CMD
         wget -q http://www.sphinxsearch.com/downloads/sphinx-0.9.8.1.tar.gz >> sphinx.log
         tar xzvf sphinx-0.9.8.1.tar.gz
@@ -42,48 +42,48 @@ DESC
         CMD
         run commands.split(/\n\s+/).join(" && ")
       end
-    
+
       desc "Install Thinking Sphinx as a gem from GitHub"
       task :ts do
         run "#{try_sudo} gem install thinking-sphinx --source http://gemcutter.org"
       end
     end
-  
+
     desc "Generate the Sphinx configuration file"
     task :configure do
       rake "thinking_sphinx:configure"
     end
-  
+
     desc "Index data"
     task :index do
       rake "thinking_sphinx:index"
     end
-  
+
     desc "Start the Sphinx daemon"
     task :start do
       configure
       rake "thinking_sphinx:start"
     end
-  
+
     desc "Stop the Sphinx daemon"
     task :stop do
       configure
       rake "thinking_sphinx:stop"
     end
-  
+
     desc "Stop and then start the Sphinx daemon"
     task :restart do
       stop
       start
     end
-  
+
     desc "Stop, re-index and then start the Sphinx daemon"
     task :rebuild do
       stop
       index
       start
     end
-  
+
     desc "Add the shared folder for sphinx files"
     task :shared_sphinx_folder, :roles => :web do
       rails_env = fetch(:rails_env, "production")
