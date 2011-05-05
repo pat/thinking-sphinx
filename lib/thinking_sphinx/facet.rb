d1baed72a27277164a83ebfecee2989f0d1b1aa0
Pat Allan <pat@freelancing-gods.com>
2011-05-05 11:39:33 +1000

    Cleaning up facet patches - line was getting a little long, and I didn't like double-evaluating the method.

--- lib/thinking_sphinx/facet.rb
+++ lib/thinking_sphinx/facet.rb
@@ -101,7 +101,10 @@ module ThinkingSphinx
       return if objects.blank?
 
       method = value_source || column.__name
-      object = objects.one? ? objects.first : objects.detect { |item| item.send(method) && item.send(method).to_crc32 == attribute_value }
+      object = objects.one? ? objects.first : objects.detect { |item|
+        result = item.send(method)
+        result && result.to_crc32 == attribute_value
+      }
 
       object.try(method)
     end
