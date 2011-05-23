b6c4c2a4dcf9c0531c5bf2b19bdf0792fb16f937
Pat Allan <pat@freelancing-gods.com>
2011-05-23 12:21:53 +1000

    Being pedantic with spacing.

--- lib/thinking_sphinx/configuration.rb
+++ lib/thinking_sphinx/configuration.rb
@@ -91,10 +91,10 @@ module ThinkingSphinx
       if custom_app_root
         self.app_root = custom_app_root
       else
-        self.app_root          = Merb.root                  if defined?(Merb)
-        self.app_root          = Sinatra::Application.root  if defined?(Sinatra)
-        self.app_root          = RAILS_ROOT                 if defined?(RAILS_ROOT)
-        self.app_root        ||= app_root
+        self.app_root   = Merb.root                  if defined?(Merb)
+        self.app_root   = Sinatra::Application.root  if defined?(Sinatra)
+        self.app_root   = RAILS_ROOT                 if defined?(RAILS_ROOT)
+        self.app_root ||= app_root
       end
       
       @configuration = Riddle::Configuration.new
