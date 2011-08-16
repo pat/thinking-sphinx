3a978c613d5400a854d1e0fce8ae5bc072c56635
Andrew White <andyw@pixeltrix.co.uk>
2011-08-16 20:09:11 +0100

    Refactor association scope support to eliminate SystemStackError

--- lib/thinking_sphinx/active_record/collection_proxy_with_scopes.rb
+++ lib/thinking_sphinx/active_record/collection_proxy_with_scopes.rb
@@ -4,23 +4,23 @@ module ThinkingSphinx
       def self.included(base)
         base.class_eval do
           alias_method_chain :method_missing, :sphinx_scopes
+          alias_method_chain :respond_to?, :sphinx_scopes
         end
       end
 
       def method_missing_with_sphinx_scopes(method, *args, &block)
-        if responds_to_scope(method)
-          proxy_association.klass.
-            search(:with => default_filter).
-            send(method, *args, &block)
+        klass = proxy_association.klass
+        if klass.respond_to?(:sphinx_scopes) && klass.sphinx_scopes.include?(method)
+          klass.search(:with => default_filter).send(method, *args, &block)
         else
           method_missing_without_sphinx_scopes(method, *args, &block)
         end
       end
 
-      private
-      def responds_to_scope(scope)
+      def respond_to_with_sphinx_scopes?(method)
         proxy_association.klass.respond_to?(:sphinx_scopes) &&
-        proxy_association.klass.sphinx_scopes.include?(scope)
+        proxy_association.klass.sphinx_scopes.include?(scope) ||
+        respond_to_without_sphinx_scopes?(method)
       end
     end
   end
