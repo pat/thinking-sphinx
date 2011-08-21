6ebefebe402fca64e24e1f27284d92b6d265146e
Pat Allan <pat@freelancing-gods.com>
2011-08-21 17:10:37 +1000

    Fixing up specs to handle AR's behaviour.

--- spec/fixtures/models.rb
+++ spec/fixtures/models.rb
@@ -22,15 +22,15 @@ end
 class Friendship < ActiveRecord::Base
   belongs_to :person
   belongs_to :friend, :class_name => "Person", :foreign_key => :friend_id
-  
+
   define_index do
     indexes "'something'", :as => :something
     has person_id, friend_id
-    
+
     set_property :latitude_attr   => :person_id
     set_property :longitude_attr  => :person_id
   end
-  
+
   sphinx_scope(:reverse) {
     {:order => "@weight ASC"}
   }
@@ -44,38 +44,38 @@ class Person < ActiveRecord::Base
   belongs_to :team, :polymorphic => :true
   belongs_to :source, :polymorphic => :true
   has_many :contacts
-  
+
   has_many :friendships
   has_many :friends, :through => :friendships
-  
+
   has_many :tags
   has_many :football_teams, :through => :tags
-  
+
   has_and_belongs_to_many :links
-  
+
   define_index do
     indexes [first_name, middle_initial, last_name], :as => :name
     indexes team.name, :as => :team_name
     indexes contacts.phone_number, :as => :phone_numbers
     indexes city,   :prefixes => true, :facet => true
     indexes state,  :infixes  => true, :facet => true
-    
+
     has [first_name, middle_initial, last_name], :as => :name_sort
     has team.name, :as => :team_name_sort
-    
+
     has [:id, :team_id], :as => :ids
     has team(:id), :as => :team_id
-    
+
     has contacts.phone_number, :as => :phone_number_sort
     has contacts(:id), :as => :contact_ids
-    
+
     has birthday, :facet => true
-    
+
     has friendships.person_id, :as => :friendly_ids
-    
+
     has :id, :as => :latitude
     has :id, :as => :longitude
-    
+
     set_property :delta => true
   end
 end
@@ -96,22 +96,22 @@ class Child < Person
 end
 
 class Alpha < ActiveRecord::Base
-  has_many :betas
+  has_many :betas, :class_name => 'Beta'
   has_many :thetas
-  
+
   define_index do
     indexes :name, :sortable => true
-    
+
     has :id, :as => :lat
     has :id, :as => :lng
-    
+
     set_property :field_weights => {"name" => 10}
   end
-  
+
   def big_name
     name.upcase
   end
-  
+
   def sphinx_attributes
     :existing
   end
@@ -119,20 +119,20 @@ end
 
 class Beta < ActiveRecord::Base
   has_many :gammas
-  
+
   define_index do
     indexes :name, :sortable => true
-    
+
     has :id, :as => :lat
     has :id, :as => :lon
-    
+
     set_property :delta => true
   end
-  
+
   def excerpts
     false
   end
-  
+
   def matching_fields
     :foo
   end
--- spec/thinking_sphinx/association_spec.rb
+++ spec/thinking_sphinx/association_spec.rb
@@ -14,7 +14,7 @@ describe ThinkingSphinx::Association do
         :active_record  => 'AR'
       )
       @non_poly_reflection = stub('reflection', :name => 'non_polly')
-      
+
       Person.stub!(:reflect_on_association => @normal_reflection)
       ThinkingSphinx::Association.stub!(
         :new                  => @normal_association,
@@ -25,27 +25,31 @@ describe ThinkingSphinx::Association do
         :new => @non_poly_reflection
       )
     end
-    
+
     it "should return an empty array if no association exists" do
       Person.stub!(:reflect_on_association => nil)
-      
+
       ThinkingSphinx::Association.children(Person, :assoc).should == []
     end
-    
+
     it "should return a single association instance in an array if assocation isn't polymorphic" do
       ThinkingSphinx::Association.children(Person, :assoc).should == [@normal_association]
     end
-    
+
     it "should return multiple association instances for polymorphic associations" do
       Person.stub!(:reflect_on_association => @poly_reflection)
-      
+
       ThinkingSphinx::Association.children(Person, :assoc).should ==
         [@normal_association, @normal_association]
     end
-    
+
     it "should generate non-polymorphic 'casted' associations for each polymorphic possibility" do
-      Person.stub!(:reflect_on_association).exactly(3).times.
-        and_return(@poly_reflection, nil, nil)
+      reflections = double('reflections', :[]= => nil)
+      Person.stub!(:reflections => reflections)
+
+      Person.should_receive(:reflect_on_association).once.
+        and_return(@poly_reflection)
+      reflections.should_receive(:[]).with(:polly_Person).twice.and_return(nil)
       ThinkingSphinx::Association.should_receive(:casted_options).with(
         Person, @poly_reflection
       ).twice
@@ -55,13 +59,18 @@ describe ThinkingSphinx::Association do
       ThinkingSphinx::Association.should_receive(:new).with(
         nil, @non_poly_reflection
       ).twice
-      
+
       ThinkingSphinx::Association.children(Person, :assoc)
     end
-    
+
     it "should use existing non-polymorphic 'casted' associations" do
-      Person.stub!(:reflect_on_association).exactly(3).times.
-        and_return(@poly_reflection, nil, @non_poly_reflection)
+      reflections = double('reflections', :[]= => nil)
+      Person.stub!(:reflections => reflections)
+
+      Person.stub!(:reflect_on_association).once.
+        and_return(@poly_reflection)
+      reflections.should_receive(:[]).with(:polly_Person).twice.
+        and_return(nil, @non_poly_reflection)
       ThinkingSphinx::Association.should_receive(:casted_options).with(
         Person, @poly_reflection
       ).once
@@ -71,30 +80,30 @@ describe ThinkingSphinx::Association do
       ThinkingSphinx::Association.should_receive(:new).with(
         nil, @non_poly_reflection
       ).twice
-      
+
       ThinkingSphinx::Association.children(Person, :assoc)
     end
   end
-  
+
   describe '#children' do
     before :each do
       @reflection   = stub('reflection', :klass => :klass)
       @association  = ThinkingSphinx::Association.new(nil, @reflection)
       ThinkingSphinx::Association.stub!(:children => :result)
     end
-    
+
     it "should return the children associations for the given association" do
       @association.children(:assoc).should == :result
     end
-    
+
     it "should request children for the reflection klass" do
       ThinkingSphinx::Association.should_receive(:children).
         with(:klass, :assoc, @association)
-      
+
       @association.children(:assoc)
     end
   end
-  
+
   describe '#join_to' do
     before :each do
       @parent_join = stub('join assoc').as_null_object
@@ -109,57 +118,57 @@ describe ThinkingSphinx::Association do
         ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinAssociation.stub!(:new => @join)
       end
     end
-    
+
     it "should call the parent's join_to if parent has no join" do
       @assoc = ThinkingSphinx::Association.new(@parent, :ref)
       @parent.should_receive(:join_to).with(@base_join)
-      
+
       @assoc.join_to(@base_join)
     end
-    
+
     it "should not call the parent's join_to if it already has a join" do
       @assoc = ThinkingSphinx::Association.new(@parent, :ref)
       @parent.stub!(:join => @parent_join)
       @parent.should_not_receive(:join_to)
-      
+
       @assoc.join_to(@base_join)
     end
-    
+
     it "should define the join association with a JoinAssociation instance" do
       @assoc = ThinkingSphinx::Association.new(@parent, :ref)
-      
+
       @assoc.join_to(@base_join).should == @join
       @assoc.join.should == @join
     end
   end
-  
+
   describe '#is_many?' do
     before :each do
       @parent = stub('assoc', :is_many? => :parent_is_many)
       @reflection = stub('reflection', :macro => :has_many)
     end
-    
+
     it "should return true if association is either a has_many or a habtm" do
       association = ThinkingSphinx::Association.new(@parent, @reflection)
       association.is_many?.should be_true
-      
+
       @reflection.stub!(:macro => :has_and_belongs_to_many)
       association.is_many?.should be_true
     end
-    
+
     it "should return the parent value if not a has many or habtm and there is a parent" do
       association = ThinkingSphinx::Association.new(@parent, @reflection)
       @reflection.stub!(:macro => :belongs_to)
       association.is_many?.should == :parent_is_many
     end
-    
+
     it "should return false if no parent and not a has many or habtm" do
       association = ThinkingSphinx::Association.new(nil, @reflection)
       @reflection.stub!(:macro => :belongs_to)
       association.is_many?.should be_false
     end
   end
-  
+
   describe '#ancestors' do
     it "should return an array of associations - including all parents" do
       parent = stub('assoc', :ancestors => [:all, :ancestors])
@@ -167,7 +176,7 @@ describe ThinkingSphinx::Association do
       association.ancestors.should == [:all, :ancestors, association]
     end
   end
-  
+
   describe '.polymorphic_classes' do
     it "should return all the polymorphic result types as classes" do
       Person.connection.stub!(:select_all => [
@@ -183,7 +192,7 @@ describe ThinkingSphinx::Association do
       ThinkingSphinx::Association.send(:polymorphic_classes, ref).should == [Person, Friendship]
     end
   end
-  
+
   describe '.casted_options' do
     before :each do
       @options = {
@@ -194,7 +203,7 @@ describe ThinkingSphinx::Association do
       @reflection = stub('assoc reflection', :options => @options)
       @reflection.stub!(:foreign_type => "thing_type") if ThinkingSphinx.rails_3_1?
     end
-    
+
     it "should return a new options set for a specific class" do
       ThinkingSphinx::Association.send(:casted_options, Person, @reflection).should == {
         :polymorphic  => nil,
@@ -204,7 +213,7 @@ describe ThinkingSphinx::Association do
         :conditions   => "::ts_join_alias::.`thing_type` = 'Person'"
       }
     end
-        
+
     it "should append to existing Array of conditions" do
       @options[:conditions] = ["first condition"]
       ThinkingSphinx::Association.send(:casted_options, Person, @reflection).should == {
@@ -215,7 +224,7 @@ describe ThinkingSphinx::Association do
         :conditions   => ["first condition", "::ts_join_alias::.`thing_type` = 'Person'"]
       }
     end
-    
+
     it "should merge to an existing Hash of conditions" do
       @options[:conditions] = {"field" => "value"}
       ThinkingSphinx::Association.send(:casted_options, Person, @reflection).should == {
@@ -226,7 +235,7 @@ describe ThinkingSphinx::Association do
         :conditions   => {"field" => "value", "thing_type" => "Person"}
       }
     end
-    
+
     it "should append to an existing String of conditions" do
       @options[:conditions] = "first condition"
       ThinkingSphinx::Association.send(:casted_options, Person, @reflection).should == {
