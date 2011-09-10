46631a97cafc5b6961c2d2bf703828bd63b03ac2
Pat Allan <pat@freelancing-gods.com>
2011-09-10 17:32:57 +1000

    Handle calls to Rails' application method in specs.

--- spec/support/rails.rb
+++ spec/support/rails.rb
@@ -2,13 +2,17 @@ module Rails
   def self.root
     File.join(Dir.pwd, 'tmp')
   end
-  
+
   def self.env
     @@environment ||= 'development'
   end
-  
+
   def self.env=(env)
     @@environment = env
+  end
+
+  def self.application
+    @@application ||= Struct.new(:paths).new(Hash.new([]))
   end
 end
 
