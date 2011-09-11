a0f5050bda972ced7ceeb3c19eddb0e83de87a20
Pat Allan <pat@freelancing-gods.com>
2011-09-11 13:54:44 +1000

    Populating the search results on demand.
    Still feels a little hacky - patches welcome to rework this so it's more elegant.

--- lib/thinking_sphinx/search.rb
+++ lib/thinking_sphinx/search.rb
@@ -1,19 +1,29 @@
 class ThinkingSphinx::Search < Array
+  CoreMethods = %w( == class class_eval extend frozen? id instance_eval
+    instance_of? instance_values instance_variable_defined?
+    instance_variable_get instance_variable_set instance_variables is_a?
+    kind_of? member? method methods nil? object_id respond_to?
+    respond_to_missing? send should should_not type )
+  SafeMethods = %w( partition private_methods protected_methods public_methods
+    send class )
+
+  instance_methods.select { |method|
+    method.to_s[/^__/].nil? && !CoreMethods.include?(method.to_s)
+  }.each { |method|
+    undef_method method
+  }
+
   attr_reader :options
 
   def initialize(query = nil, options = {})
     @query, @options = query, options
-  end
-
-  def empty?
-    populate
-    super
+    @array           = []
   end
 
   def populate
     return if @populated
 
-    replace connection.query(sphinxql_select.to_sql).collect { |row|
+    @array.replace connection.query(sphinxql_select.to_sql).collect { |row|
       row['sphinx_internal_class'].constantize.find row['sphinx_internal_id']
     }
     @populated = true
@@ -33,6 +43,12 @@ class ThinkingSphinx::Search < Array
     @connection ||= Riddle::Query.connection(
       (config.searchd.address || '127.0.0.1'), config.searchd.mysql41
     )
+  end
+
+  def method_missing(method, *args, &block)
+    populate if !SafeMethods.include?(method.to_s)
+
+    @array.send(method, *args, &block)
   end
 
   def sphinxql_select
--- spec/acceptance/searching_within_a_model_spec.rb
+++ spec/acceptance/searching_within_a_model_spec.rb
@@ -2,14 +2,11 @@ require 'spec_helper'
 
 describe 'Searching within a model', :live => true do
   it "returns results" do
+    article = nil
     index do
-      Article.create! :title => 'Pancakes'
+      article = Article.create! :title => 'Pancakes'
     end
 
-    articles = Article.search
-
-    articles.populate
-    articles.length.should == 1
-    articles.first.should be_an(Article)
+    Article.search.first.should == article
   end
 end
