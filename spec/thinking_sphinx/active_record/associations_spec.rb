29073ed0fc0999c3667a45fd83f94ab9a8362ad0
Pat Allan <pat@freelancing-gods.com>
2011-09-18 10:41:14 +1000

    Adding unit specs for Associations class.

--- spec/thinking_sphinx/active_record/associations_spec.rb
+++ spec/thinking_sphinx/active_record/associations_spec.rb
@@ -1,31 +1,100 @@
 require 'spec_helper'
 
 describe ThinkingSphinx::ActiveRecord::Associations do
+  let(:model)        {
+    double('model', :quoted_table_name => 'articles', :reflections => {})
+  }
+  let(:associations) { ThinkingSphinx::ActiveRecord::Associations.new model }
+  let(:base)         {
+    double('base', :active_record => model, :join_base => join_base)
+  }
+  let(:join_base)    { double('join base') }
+  let(:reflection)   { double('reflection') }
+  let(:join)         {
+    double('join', :join_type= => nil, :aliased_table_name => 'users')
+  }
+  let(:sub_reflection) { double('sub reflection') }
+  let(:sub_join)       {
+    double('sub join', :join_type= => nil, :aliased_table_name => 'posts')
+  }
+
+  before :each do
+    ActiveRecord::Associations::JoinDependency.stub! :new => base
+    ActiveRecord::Associations::JoinDependency::JoinAssociation.
+      stub! :new => join
+    model.reflections[:user] = reflection
+
+    join.stub!(
+      :active_record => double('model', :reflections => {}),
+      :join          => double('joiner')
+    )
+    join.active_record.reflections[:posts] = sub_reflection
+  end
+
   describe '#alias_for' do
     it "returns the model's table name when no stack is given" do
-      pending
+      associations.alias_for([]).should == 'articles'
     end
 
     it "adds just one join for a stack with a single association" do
-      pending
+      ActiveRecord::Associations::JoinDependency::JoinAssociation.
+        should_receive(:new).with(reflection, base, join_base).once.
+        and_return(join)
+
+      associations.alias_for([:user])
     end
 
-    it "adds two joins for a stack with two associations" do
-      pending
+    it "returns the aliased table name for the join" do
+      associations.alias_for([:user]).should == 'users'
     end
 
     it "does not duplicate joins when given the same stack twice" do
-      pending
+      ActiveRecord::Associations::JoinDependency::JoinAssociation.
+        should_receive(:new).once.and_return(join)
+
+      associations.alias_for([:user])
+      associations.alias_for([:user])
     end
 
-    it "extends upon existing joins when given stacks where parts are already mapped" do
-      pending
+    context 'multiple joins' do
+      it "adds two joins for a stack with two associations" do
+        ActiveRecord::Associations::JoinDependency::JoinAssociation.
+          should_receive(:new).with(reflection, base, join_base).once.
+          and_return(join)
+
+        ActiveRecord::Associations::JoinDependency::JoinAssociation.
+          should_receive(:new).with(sub_reflection, base, join.join).once.
+          and_return(sub_join)
+
+        associations.alias_for([:user, :posts])
+      end
+
+      it "returns the sub join's aliased table name" do
+        ActiveRecord::Associations::JoinDependency::JoinAssociation.
+          stub(:new).and_return(join, sub_join)
+
+        associations.alias_for([:user, :posts]).should == 'posts'
+      end
+
+      it "extends upon existing joins when given stacks where parts are already mapped" do
+        ActiveRecord::Associations::JoinDependency::JoinAssociation.
+          should_receive(:new).twice.and_return(join, sub_join)
+
+        associations.alias_for([:user])
+        associations.alias_for([:user, :posts])
+      end
     end
   end
 
   describe '#join_values' do
     it "returns all joins that have been created" do
-      pending
+      ActiveRecord::Associations::JoinDependency::JoinAssociation.
+        stub(:new).and_return(join, sub_join)
+
+      associations.alias_for([:user])
+      associations.alias_for([:user, :posts])
+
+      associations.join_values.should == [join, sub_join]
     end
   end
 end
