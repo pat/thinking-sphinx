4c4cb87febedd322ea2b38d5a45b6e5c5e2ab1db
Pat Allan <pat@freelancing-gods.com>
2011-11-05 11:38:46 +0200

    Adding rake task descriptions.

--- lib/thinking_sphinx/tasks.rb
+++ lib/thinking_sphinx/tasks.rb
@@ -1,23 +1,30 @@
 namespace :ts do
+  desc 'The current version of Thinking Sphinx'
   task :version do
     puts "Thinking Sphinx v#{ThinkingSphinx::VERSION}"
   end
 
+  desc 'Generate the Sphinx configuration file'
   task :configure => :environment do
     interface.configure
   end
 
+  desc 'Generate the Sphinx configuration file and process all indices'
   task :index => :environment do
     interface.index
   end
 
+  desc 'Stop Sphinx, index and then restart Sphinx'
   task :rebuild => [:stop, :index, :start]
+  desc 'Restart the Sphinx daemon'
   task :restart => [:stop, :start]
 
+  desc 'Start the Sphinx daemon'
   task :start => :environment do
     interface.start
   end
 
+  desc 'Stop the Sphinx daemon'
   task :stop => :environment do
     interface.stop
   end
