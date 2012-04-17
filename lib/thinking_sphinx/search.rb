becb9568024fb5cd8b85138d46e843d893f6ec7f
Andrew Hunter <andrew.hunter@livingsocial.com>
2012-04-17 14:56:52 -0400

    Added feature to be able to specify hard_retry_count if a Sphinx search catches any kind of exception

--- lib/thinking_sphinx/search.rb
+++ lib/thinking_sphinx/search.rb
@@ -420,27 +420,40 @@ module ThinkingSphinx
     def populate
       return if @populated
       @populated = true
+      retries   = hard_retries
 
-      retry_on_stale_index do
-        begin
-          log query do
-            @results = client.query query, indexes, comment
+      begin
+        retry_on_stale_index do
+          begin
+            log query do
+              @results = client.query query, indexes, comment
+            end
+            total = @results[:total_found].to_i
+            log "Found #{total} result#{'s' unless total == 1}"
+
+            log "Sphinx Daemon returned warning: #{warning}" if warning?
+
+            if error?
+              log "Sphinx Daemon returned error: #{error}"
+              raise SphinxError.new(error, @results) unless options[:ignore_errors]
+            end
+          rescue Errno::ECONNREFUSED => err
+            raise ThinkingSphinx::ConnectionError,
+              'Connection to Sphinx Daemon (searchd) failed.'
           end
-          total = @results[:total_found].to_i
-          log "Found #{total} result#{'s' unless total == 1}"
 
-          log "Sphinx Daemon returned warning: #{warning}" if warning?
-
-          if error?
-            log "Sphinx Daemon returned error: #{error}"
-            raise SphinxError.new(error, @results) unless options[:ignore_errors]
-          end
-        rescue Errno::ECONNREFUSED => err
-          raise ThinkingSphinx::ConnectionError,
-            'Connection to Sphinx Daemon (searchd) failed.'
+          compose_results
         end
-
-        compose_results
+      rescue => e
+        log 'Caught Sphinx exception: %s (%s %s left)' % [
+          e.message, retries, (retries == 1 ? 'try' : 'tries')
+        ]
+        retries -= 1
+        if retries >= 0
+          retry
+        else
+          raise e
+        end
       end
     end
 
@@ -859,6 +872,10 @@ module ThinkingSphinx
       else
         options[:retry_stale].to_i
       end
+    end
+
+    def hard_retries
+      options[:hard_retry_count] ? options[:hard_retry_count] : config.hard_retry_count
     end
 
     def include_for_class(klass)
