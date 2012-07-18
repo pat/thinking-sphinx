c85940e0c45c3297ef6c5bb1c1dc7bca17e9c3d7
Andrew White <andyw@pixeltrix.co.uk>
2012-07-18 13:04:47 +0100

    Add support for association-filtered faceting
    It's a common use case that faceting needs to be scoped to a subset.
    Whilst this can be done manually, it's easy to extend the built-in
    support for association-filtered searching to faceting as well, e.g:
    
    Currently:
    
      @facets = Product.facets :with => { :manufacturer_id => @manufacturer.id }
    
    After this commit:
    
      @facets = @manufacturer.products.facets
    
    Whilst technically this changes current behavior the  difference
    between searching and faceting through an association is inconsistent
    and I feel it should be considered a bug.

--- spec/thinking_sphinx/active_record/has_many_association_spec.rb
+++ spec/thinking_sphinx/active_record/has_many_association_spec.rb
@@ -41,6 +41,46 @@ describe 'ThinkingSphinx::ActiveRecord::HasManyAssociation' do
     end
   end
   
+  describe "facets method" do
+    before :each do
+      Friendship.stub!(:facets => true)
+
+      @person = Person.find(:first)
+      @index  = Friendship.sphinx_indexes.first
+    end
+
+    it "should raise an error if the required attribute doesn't exist" do
+      @index.stub!(:attributes => [])
+
+      lambda { @person.friendships.facets "test" }.should raise_error(RuntimeError)
+    end
+
+    it "should add a filter for the attribute into a normal facets call" do
+      Friendship.should_receive(:facets) do |query, options|
+        options[:with][:person_id].should == @person.id
+      end
+
+      @person.friendships.facets "test"
+    end
+
+    it "should add a filter for an aliased attribute into a normal facets call" do
+      @team = CricketTeam.new
+      @team.stub!(:id => 1)
+
+      Person.should_receive(:facets).with do |query, options|
+        options[:with][:team_id].should == @team.id
+      end
+
+      @team.people.facets "test"
+    end
+
+    it "should define indexes for the reflection class" do
+      Friendship.should_receive(:define_indexes)
+
+      @person.friendships.facets 'test'
+    end
+  end
+
   describe "search method for has_many :through" do
     before :each do
       Person.stub!(:search => true)
@@ -75,6 +115,40 @@ describe 'ThinkingSphinx::ActiveRecord::HasManyAssociation' do
     end
   end
   
+  describe "facets method for has_many :through" do
+    before :each do
+      Person.stub!(:facets => true)
+
+      @person = Person.find(:first)
+      @index  = Person.sphinx_indexes.first
+    end
+
+    it "should raise an error if the required attribute doesn't exist" do
+      @index.stub!(:attributes => [])
+
+      lambda { @person.friends.facets "test" }.should raise_error(RuntimeError)
+    end
+
+    it "should add a filter for the attribute into a normal facets call" do
+      Person.should_receive(:facets).with do |query, options|
+        options[:with][:friendly_ids].should == @person.id
+      end
+
+      @person.friends.facets "test"
+    end
+
+    it "should add a filter for an aliased attribute into a normal facets call" do
+      @team = FootballTeam.new
+      @team.stub!(:id => 1)
+
+      Person.should_receive(:facets).with do |query, options|
+        options[:with][:football_team_id].should == @team.id
+      end
+
+      @team.people.facets "test"
+    end
+  end
+
   describe 'filtering sphinx scopes' do
     before :each do
       Friendship.stub!(:search => Friendship)
