15908474e33729cd7b6c3726578c7603409d3203
Pat Allan <pat@freelancing-gods.com>
2012-08-10 22:08:37 +0100

    1.4.13

--- lib/thinking_sphinx/bundled_search.rb
+++ lib/thinking_sphinx/bundled_search.rb
@@ -1,41 +1,41 @@
 module ThinkingSphinx
   class BundledSearch
     attr_reader :client
-    
+
     def initialize
       @searches = []
     end
-    
+
     def search(*args)
       @searches << ThinkingSphinx.search(*args)
       @searches.last.append_to client
     end
-    
+
     def search_for_ids(*args)
       @searches << ThinkingSphinx.search_for_ids(*args)
       @searches.last.append_to client
     end
-    
+
     def searches
       populate
       @searches
     end
-    
+
     private
-    
+
     def client
       @client ||= ThinkingSphinx::Configuration.instance.client
     end
-    
+
     def populated?
       @populated
     end
-    
+
     def populate
       return if populated?
-      
+
       @populated = true
-      
+
       client.run.each_with_index do |results, index|
         searches[index].populate_from_queue results
       end
--- lib/thinking_sphinx/version.rb
+++ lib/thinking_sphinx/version.rb
@@ -1,3 +1,3 @@
 module ThinkingSphinx
-  Version = '1.4.12'
+  Version = '1.4.13'
 end
