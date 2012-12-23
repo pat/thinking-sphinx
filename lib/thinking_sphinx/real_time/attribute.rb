ef30626f2f81359ecc67f29c25f166ed07ca4e73
Pat Allan <pat@freelancing-gods.com>
2012-12-23 22:46:44 +1100

    Smart overrides for real-time property default values.

--- lib/thinking_sphinx/real_time/attribute.rb
+++ lib/thinking_sphinx/real_time/attribute.rb
@@ -2,4 +2,14 @@ class ThinkingSphinx::RealTime::Attribute < ThinkingSphinx::RealTime::Property
   def type
     @options[:type]
   end
+
+  def translate(object)
+    super || default_value
+  end
+
+  private
+
+  def default_value
+    type == :string ? '' : 0
+  end
 end
--- lib/thinking_sphinx/real_time/field.rb
+++ lib/thinking_sphinx/real_time/field.rb
@@ -1,5 +1,7 @@
 class ThinkingSphinx::RealTime::Field < ThinkingSphinx::RealTime::Property
   include ThinkingSphinx::Core::Field
 
-  #
+  def translate(object)
+    Array(super || '').join(' ').gsub(/\s+/, ' ')
+  end
 end
