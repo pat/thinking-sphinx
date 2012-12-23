34661dba88fc8b46f5f4e5198c0e28644df9e9f3
Pat Allan <pat@freelancing-gods.com>
2012-12-23 22:59:48 +1100

    Riddle handles this for us.

--- lib/thinking_sphinx/real_time/field.rb
+++ lib/thinking_sphinx/real_time/field.rb
@@ -2,6 +2,6 @@ class ThinkingSphinx::RealTime::Field < ThinkingSphinx::RealTime::Property
   include ThinkingSphinx::Core::Field
 
   def translate(object)
-    Array(super || '').join(' ').gsub(/\s+/, ' ')
+    Array(super || '').join(' ')
   end
 end
