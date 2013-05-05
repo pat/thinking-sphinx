1fcafe10ef31a7bc7eed9a413277e64233a64b36
Pat Allan <pat@freelancing-gods.com>
2013-05-05 11:57:02 -0700

    Fixing deprecated default_scope syntax.

--- spec/internal/app/models/user.rb
+++ spec/internal/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles
 
-  default_scope order(:id)
+  default_scope { order(:id) }
   scope :recent, lambda { where('created_at > ?', 1.week.ago) }
 end
