21a490ae675c7f75783d1a302aa8b763ae87f299
Дмитрий Карбышев <d.karbyshev@i-free.com>
2013-11-01 12:49:10 +0400

    Gather database connection details from model if it has its own, otherwise take connection details from ActiveRecord. Allows transparently use specific model database settings with sphinx

--- lib/thinking_sphinx/active_record/sql_source.rb
+++ lib/thinking_sphinx/active_record/sql_source.rb
@@ -11,8 +11,11 @@ module ThinkingSphinx
 
       def initialize(model, options = {})
         @model             = model
-        @database_settings = ::ActiveRecord::Base.connection.
-          instance_variable_get(:@config).clone
+        @database_settings =
+          model.connection.instance_variable_get(:@config) ===
+          ::ActiveRecord::Base.connection.instance_variable_get(:@config) ?
+          ::ActiveRecord::Base.connection.instance_variable_get(:@config).clone :
+          model.connection.instance_variable_get(:@config).clone
         @options           = {
           :utf8? => (@database_settings[:encoding] == 'utf8')
         }.merge options
