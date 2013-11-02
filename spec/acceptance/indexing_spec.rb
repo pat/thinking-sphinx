1f109979ce0718ddd83562a3248d1675f3d4dd44
Pat Allan <pat@freelancing-gods.com>
2013-11-02 13:41:49 +1100

    Avoid concurrent indexing via temporary files.

---
+++ spec/acceptance/indexing_spec.rb
@@ -0,0 +1,39 @@
+require 'acceptance/spec_helper'
+
+describe 'Indexing', :live => true do
+  it "does not index files where the temp file exists" do
+    path = Rails.root.join('db/sphinx/test/ts-article_core.tmp')
+    FileUtils.mkdir_p Rails.root.join('db/sphinx/test')
+    FileUtils.touch path
+
+    article = Article.create! :title => 'Pancakes'
+    index 'article_core'
+    Article.search.should be_empty
+
+    FileUtils.rm path
+  end
+
+  it "does not index files when all indices are already being processed" do
+    path = Rails.root.join('db/sphinx/test/ts---all.tmp')
+    FileUtils.mkdir_p Rails.root.join('db/sphinx/test')
+    FileUtils.touch path
+
+    article = Article.create! :title => 'Pancakes'
+    index 'article_core'
+    Article.search.should be_empty
+
+    FileUtils.rm path
+  end
+
+  it "indexes files when other indices are already being processed" do
+    path = Rails.root.join('db/sphinx/test/ts-book_core.tmp')
+    FileUtils.mkdir_p Rails.root.join('db/sphinx/test')
+    FileUtils.touch path
+
+    article = Article.create! :title => 'Pancakes'
+    index 'article_core'
+    Article.search.should_not be_empty
+
+    FileUtils.rm path
+  end
+end
