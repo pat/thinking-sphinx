504e68b9585f20525575bbdffad8c72d6a665cc3
Timo Virkkala <timo.virkkala@agiilit.com>
2013-11-07 15:45:56 +0200

    Added failing test - FacetSearch uses limit or per_page for max_matches, which prevents pagination from working properly

--- spec/thinking_sphinx/facet_search_spec.rb
+++ spec/thinking_sphinx/facet_search_spec.rb
@@ -3,6 +3,8 @@ module ThinkingSphinx; end
 require 'thinking_sphinx/facet_search'
 require 'thinking_sphinx/facet'
 
+require 'spec_helper'
+
 describe ThinkingSphinx::FacetSearch do
   let(:facet_search)  { ThinkingSphinx::FacetSearch.new '', {} }
   let(:batch)         { double('batch', :searches => [], :populate => true) }
@@ -117,6 +119,18 @@ describe ThinkingSphinx::FacetSearch do
         batch.searches.each { |search|
           search.options[setting].should == 42
         }
+      end
+
+      it "allows separate #{setting} and max_matches settings to support pagination" do
+        configuration.settings['max_matches'] = 500
+        facet_search = ThinkingSphinx::FacetSearch.new '', {setting => 10}
+
+        facet_search.populate
+
+        batch.searches.each do |search|
+          search.options[setting].should == 10
+          search.options[:max_matches].should == 500
+        end
       end
     end
   end
