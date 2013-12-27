7983ed63eba5d2c3c2acb101fbf06a687413d98a
Pat Allan <pat@freelancing-gods.com>
2013-12-27 18:37:41 +1100

    Checking UTF8 setting properly (default/nil is true now).

--- lib/thinking_sphinx/middlewares/utf8.rb
+++ lib/thinking_sphinx/middlewares/utf8.rb
@@ -5,12 +5,17 @@ class ThinkingSphinx::Middlewares::UTF8 <
     contexts.each do |context|
       context[:results].each { |row| update_row row }
       update_row context[:meta]
-    end unless ThinkingSphinx::Configuration.instance.settings['utf8']
+    end unless encoded?
 
     app.call contexts
   end
 
   private
+
+  def encoded?
+    ThinkingSphinx::Configuration.instance.settings['utf8'].nil? ||
+    ThinkingSphinx::Configuration.instance.settings['utf8']
+  end
 
   def update_row(row)
     row.each do |key, value|
