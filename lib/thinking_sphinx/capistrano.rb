22754fcdc597e041620e8a52a8807f9e750146dc
Alexander Tipugin <atipugin@gmail.com>
2013-12-27 12:33:21 +0400

    Clean up Capistrano version-dependent recipe loading
    Conflicts:
    	lib/thinking_sphinx/capistrano.rb

--- lib/thinking_sphinx/capistrano.rb
+++ lib/thinking_sphinx/capistrano.rb
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-Capistrano::Configuration.instance(:must_exist).load do
-if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
-  load File.expand_path("../tasks/capistrano.rake", __FILE__)
-else
-  require_relative 'capistrano2'
+if defined?(Capistrano::VERSION)
+  major_version = Gem::Version.new(Capistrano::VERSION).segments.first
+  require_relative "capistrano/v#{major_version}"
 end
