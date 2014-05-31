94341b7b7f5d8f5f209945a6ea8a4473608a16b4
Pat Allan <pat@freelancing-gods.com>
2014-05-31 11:48:19 +1000

    Rebuild now uses the clear task between stopping the daemon and indexing.
    This ensures TS index locking files are removed as well. Should help situations like in #778.

--- lib/thinking_sphinx/tasks.rb
+++ lib/thinking_sphinx/tasks.rb
@@ -24,7 +24,7 @@ namespace :ts do
   end
 
   desc 'Stop Sphinx, index and then restart Sphinx'
-  task :rebuild => [:stop, :index, :start]
+  task :rebuild => [:stop, :clear, :index, :start]
 
   desc 'Stop Sphinx, clear files, reconfigure, start Sphinx, generate files'
   task :regenerate => [:stop, :clear, :configure, :start, :generate]
