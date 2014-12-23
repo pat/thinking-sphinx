87928c9298111818c9dd42e8c7faebfc8ae41c0b
Pat Allan <pat@freelancing-gods.com>
2014-12-24 00:30:51 +1100

    Switch from ActiveSupport's on_load to railtie initializer.
    This resolves the insistent warning about errors in transactions, covered in #867.

--- lib/thinking_sphinx/railtie.rb
+++ lib/thinking_sphinx/railtie.rb
@@ -1,6 +1,8 @@
 class ThinkingSphinx::Railtie < Rails::Railtie
-  ActiveSupport.on_load :active_record do
-    include ThinkingSphinx::ActiveRecord::Base
+  initializer 'thinking_sphinx.initialisation' do
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.send :include, ThinkingSphinx::ActiveRecord::Base
+    end
   end
 
   rake_tasks do
