45a0696311c3d8c17b23084cb4792585babc188c
Pat Allan <pat@freelancing-gods.com>
2014-12-29 23:14:35 +1100

    Add required timestamps null option for Rails 4.2.

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Schema.define do
   create_table(:admin_people, :force => true) do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:animals, :force => true) do |t|
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define do
     t.text    :content
     t.boolean :published
     t.integer :user_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:manufacturers, :force => true) do |t|
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define do
     t.string  :blurb_file
     t.boolean :delta, :default => true,   :null => false
     t.string  :type,  :default => 'Book', :null => false
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:books_genres, :force => true, :id => false) do |t|
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define do
 
   create_table(:colours, :force => true) do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:events, :force => true) do |t|
@@ -77,27 +77,27 @@ ActiveRecord::Schema.define do
   create_table(:taggings, :force => true) do |t|
     t.integer :tag_id
     t.integer :article_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:tags, :force => true) do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:tees, :force => true) do |t|
     t.integer :colour_id
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:tweets, :force => true, :id => false) do |t|
     t.column :id, :bigint, :null => false
     t.string :text
-    t.timestamps
+    t.timestamps null: false
   end
 
   create_table(:users, :force => true) do |t|
     t.string :name
-    t.timestamps
+    t.timestamps null: false
   end
 end
