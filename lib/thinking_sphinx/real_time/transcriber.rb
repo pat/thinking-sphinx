1fc8d159d8c056abd97635acffc70462f68e557a
Pat Allan <pat@freelancing-gods.com>
2015-11-18 20:56:23 +0100

    Cleaning up instance transcription logic.
    Using a separate object per instance to reduce the compexity of the copy method.

--- lib/thinking_sphinx/real_time/transcriber.rb
+++ lib/thinking_sphinx/real_time/transcriber.rb
@@ -4,18 +4,14 @@ class ThinkingSphinx::RealTime::Transcriber
   end
 
   def copy(*instances)
-    items = instances.flatten.select { |instance| instance.persisted? && copy?(instance) }
+    items = instances.select { |instance|
+      instance.persisted? && copy?(instance)
+    }
     return unless items.present?
-    properties = (index.fields + index.attributes)
-    values = []
-    columns = properties.each_with_object(['id']) do |property, columns|
-      columns << property.name
-    end
-    items.each do |instance|
-      values << properties.each_with_object([index.document_id_for_key(instance.id)]) do |property, instance_values|
-        instance_values << property.translate(instance)
-      end
-    end
+
+    values = items.collect { |instance|
+      TranscribeInstance.call(instance, index, properties)
+    }
 
     insert = Riddle::Query::Insert.new index.name, columns, values
     sphinxql = insert.replace!.to_sql
@@ -23,13 +19,43 @@ class ThinkingSphinx::RealTime::Transcriber
     ThinkingSphinx::Logger.log :query, sphinxql do
       ThinkingSphinx::Connection.take do |connection|
         connection.execute sphinxql
-      end 
+      end
+    end
+  end
+
+  class TranscribeInstance
+    def self.call(instance, index, properties)
+      new(instance, index, properties).call
+    end
+
+    def initialize(instance, index, properties)
+      @instance, @index, @properties = instance, index, properties
+    end
+
+    def call
+      properties.each_with_object([document_id]) do |property, instance_values|
+        instance_values << property.translate(instance)
+      end
+    end
+
+    private
+
+    attr_reader :instance, :index, :properties
+
+    def document_id
+      index.document_id_for_key instance.id
     end
   end
 
   private
 
   attr_reader :index
+
+  def columns
+    @columns ||= properties.each_with_object(['id']) do |property, columns|
+      columns << property.name
+    end
+  end
 
   def copy?(instance)
     index.conditions.empty? || index.conditions.all? { |condition|
@@ -42,5 +68,9 @@ class ThinkingSphinx::RealTime::Transcriber
         "Unexpected condition: #{condition}. Expecting Symbol or Proc."
       end
     }
+  end
+
+  def properties
+    @properties ||= index.fields + index.attributes
   end
 end
