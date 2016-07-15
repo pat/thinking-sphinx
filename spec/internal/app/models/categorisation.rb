c503c805ceaeb76ce9ea9cf57433f829d8edf822
Pat Allan <pat@freelancing-gods.com>
2016-07-15 17:04:02 +1000

    Working around change of Rails 5 foreign ids cache.

--- spec/internal/app/models/categorisation.rb
+++ spec/internal/app/models/categorisation.rb
@@ -2,5 +2,12 @@ class Categorisation < ActiveRecord::Base
   belongs_to :category
   belongs_to :product
 
-  after_save ThinkingSphinx::RealTime.callback_for(:product, [:product])
+  after_commit :update_product
+
+  private
+
+  def update_product
+    product.reload
+    ThinkingSphinx::RealTime.callback_for(:product, [:product]).after_save self
+  end
 end
