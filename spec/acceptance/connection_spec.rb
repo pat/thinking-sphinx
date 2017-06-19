93744da743dba267ff1adb4569c8ea6e12d3d3ed
Pat Allan <pat@freelancing-gods.com>
2017-06-19 22:54:53 +1000

    Fix frozen string literal in spec. No, really.
    If I'm going to put a spec fix in, it would make sense to run the spec and confirm it's actually fixed.

--- spec/acceptance/connection_spec.rb
+++ spec/acceptance/connection_spec.rb
@@ -2,13 +2,13 @@ require 'acceptance/spec_helper'
 
 RSpec.describe 'Connections', :live => true do
   let(:maximum)    { (2 ** 23) - 5 }
-  let(:query)      { "SELECT * FROM book_core WHERE MATCH('')" }
+  let(:query)      { String.new "SELECT * FROM book_core WHERE MATCH('')" }
   let(:difference) { maximum - query.length }
 
   it 'allows normal length queries through' do
     expect {
       ThinkingSphinx::Connection.take do |connection|
-        connection.execute query.insert(-3, String.new('a') * difference)
+        connection.execute query.insert(-3, 'a' * difference)
       end
     }.to_not raise_error
   end
@@ -16,7 +16,7 @@ RSpec.describe 'Connections', :live => true do
   it 'does not allow overly long queries' do
     expect {
       ThinkingSphinx::Connection.take do |connection|
-        connection.execute query.insert(-3, String.new('a') * (difference + 5))
+        connection.execute query.insert(-3, 'a' * (difference + 5))
       end
     }.to raise_error(ThinkingSphinx::QueryLengthError)
   end
