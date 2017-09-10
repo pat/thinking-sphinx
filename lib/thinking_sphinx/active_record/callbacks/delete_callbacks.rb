ec35be9017e18172f08b5971cd51e9effbdebe08
Pat Allan <pat@freelancing-gods.com>
2017-09-10 19:09:33 +0200

    Allow use of deletion callback for rollback events.
    Related to #1021.

--- lib/thinking_sphinx/active_record/callbacks/delete_callbacks.rb
+++ lib/thinking_sphinx/active_record/callbacks/delete_callbacks.rb
@@ -1,17 +1,25 @@
 class ThinkingSphinx::ActiveRecord::Callbacks::DeleteCallbacks <
   ThinkingSphinx::Callbacks
 
-  callbacks :after_destroy
+  callbacks :after_destroy, :after_rollback
 
   def after_destroy
+    delete_from_sphinx
+  end
+
+  def after_rollback
+    delete_from_sphinx
+  end
+
+  private
+
+  def delete_from_sphinx
     return if ThinkingSphinx::Callbacks.suspended? || instance.new_record?
 
     indices.each { |index|
       ThinkingSphinx::Deletion.perform index, instance.id
     }
   end
-
-  private
 
   def indices
     ThinkingSphinx::Configuration.instance.index_set_class.new(
