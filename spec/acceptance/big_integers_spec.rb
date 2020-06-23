c8e91478cd2c1933cadcdef8b90d14c5d4eda93e
Pat Allan <pat@freelancing-gods.com>
2020-06-23 21:44:12 +1000

    Switch update_attributes to update.
    All supported Rails versions should be fine with this.

--- spec/acceptance/big_integers_spec.rb
+++ spec/acceptance/big_integers_spec.rb
@@ -52,7 +52,7 @@ describe '64 bit document ids', :live => true do
   context 'with Real-Time' do
     it 'handles large 32 bit integers with an offset multiplier' do
       product = Product.create! :name => "Widget"
-      product.update_attributes :id => 980190962
+      product.update :id => 980190962
       expect(
         Product.search('widget', :indices => ['product_core']).to_a
       ).to eq([product])
