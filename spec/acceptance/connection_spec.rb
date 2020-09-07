2556645de2d1fd115581ff59cdf4e68d37deae70
Sandro Kalbermatter <sandro.kalbermatter@sitrox.com>
2020-09-07 17:59:03 +0200

    Implement "Adding a spec to spec/acceptance/connection_spec.rb"

--- spec/acceptance/connection_spec.rb
+++ spec/acceptance/connection_spec.rb
@@ -22,4 +22,13 @@ RSpec.describe 'Connections', :live => true do
       end
     }.to raise_error(ThinkingSphinx::QueryLengthError)
   end
+
+  it 'does not allow queries longer than specified in the settings' do
+    write_configuration('maximum_statement_length' => maximum - 5)
+    expect {
+      ThinkingSphinx::Connection.take do |connection|
+        connection.execute query.insert(-3, 'a' * difference)
+      end
+    }.to raise_error(ThinkingSphinx::QueryLengthError)
+  end
 end
